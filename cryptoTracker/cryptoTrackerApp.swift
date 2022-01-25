3820b31a7f1ee97b762b99b6d199a92a6a153abb
Dumitru Paraschiv <dumitruparaschiv@182-Dumitru-Paraschiv-iMac.local>
2022-01-25 11:36:39 +0200

    Initial Commit

---
+++ cryptoTracker/cryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoTrackerApp.swift
+//  cryptoTracker
+//
+//  Created by Dumitru Paraschiv on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct cryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
